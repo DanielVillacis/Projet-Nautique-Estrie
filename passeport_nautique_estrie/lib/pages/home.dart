81254f963810cdaf0f3b12007687257809aeaf47
Daniel Villacis Desktop <daniel1997v@gmail.com>
2024-02-23 00:22:28 -0500

    AppBar added in home page

--- passeport_nautique_estrie/lib/pages/home.dart
+++ passeport_nautique_estrie/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +7,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BreakFast'),
-      ),
+      appBar: appBar(),
     );
   }
+}
+
+AppBar appBar() {
+  return AppBar(
+    title: Text(
+      'Accueil',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 26,
+        fontWeight: FontWeight.w200,
+        fontFamily: 'Poppins-Light',
+      ),
+    ),
+    backgroundColor: Color(0xFF3A7667),
+    centerTitle: false,
+    leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(16),
+          child: SvgPicture.asset(
+            'Assets/icons/menu-svgrepo-com.svg',
+            height: 30,
+            width: 30,
+            color: Color.fromARGB(255, 31, 62, 54),
+          ),
+          decoration: BoxDecoration(color: Color(0xFF3A7667)),
+        )),
+    actions: [
+      GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            child: SvgPicture.asset(
+              'Assets/icons/notification-svgrepo-com.svg',
+              height: 30,
+              width: 30,
+              color: Color.fromARGB(255, 31, 62, 54),
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xFF3A7667),
+            ),
+          )),
+    ],
+  );
 }
