b985d7f0dc31177697e8f9b2b77df543a4019479
xPurf <xavier@vallieres.ca>
2024-03-21 22:34:44 -0400

    added DB connection

--- passeport_nautique_estrie/lib/app_state.dart
+++ passeport_nautique_estrie/lib/app_state.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:passeport_nautique_estrie/login.dart';
 import 'package:passeport_nautique_estrie/main.dart';
 import 'package:passeport_nautique_estrie/profile.dart';
+import 'package:postgres/postgres.dart';
 
 class MyAppState extends State<MyApp> {
   Credentials? _credentials;
@@ -38,8 +39,8 @@ class MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
-    auth0 = Auth0('dev-lmexjjh8yiz0b2mi.us.auth0.com',
-        '8GwTucxYUBqotD7WUwNueykcsb6uAR32');
+    auth0 = Auth0('{domain}',
+        '{client.id}');
     errorMessage = '';
   }
 
@@ -50,6 +51,7 @@ class MyAppState extends State<MyApp> {
       _credentials = null;
     });
   }
+
   Future<void> loginAction() async {
     setState(() {
       isBusy = true;
@@ -63,6 +65,18 @@ class MyAppState extends State<MyApp> {
       setState(() {
         isBusy = false;
         _credentials = credentials;
+      });
+      final conn = await Connection.open(Endpoint(
+        host: 'ep-divine-dew-a56c72dk.us-east-2.aws.neon.tech',
+        port: 5432,
+        database: 'PNE',
+        username: 'PNE_owner',
+        password: 'mJz7Re5jZVdl',
+      ));
+      conn.execute(Sql.named('CALl login(@sub, @nom, @prenom)'), parameters: {
+        'sub': _credentials?.user.sub,
+        'nom': _credentials?.user.familyName,
+        'prenom': _credentials?.user.givenName
       });
     } on Exception catch (e, s) {
       debugPrint('login error: $e - stack: $s');
