0e54e6f884b3cfc69ad4431faec3712efd091da8
Daniel Villacis Desktop <daniel1997v@gmail.com>
2024-04-15 22:21:36 -0400

    removed the debug banner

--- passeport_nautique_estrie/lib/main.dart
+++ passeport_nautique_estrie/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'PNE',
       theme: ThemeData(
         // Define your app's theme here
